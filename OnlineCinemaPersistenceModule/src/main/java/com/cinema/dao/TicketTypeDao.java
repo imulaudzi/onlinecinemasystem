07f1c3929afc82fbfdecbf387e1957a2dab73789
isaac <imulaudzi@gmail.com>
2016-02-05 23:11:04 +0200

    commit 6

--- OnlineCinemaPersistenceModule/src/main/java/com/cinema/dao/TicketTypeDao.java
+++ OnlineCinemaPersistenceModule/src/main/java/com/cinema/dao/TicketTypeDao.java
@@ -3,9 +3,14 @@ package com.cinema.dao;
 import com.cinema.dao.generic.GenericDao;
 import com.cinema.dao.model.TicketType;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 10/29/2015.
  */
 public interface TicketTypeDao extends GenericDao<TicketType,Long> {
+
+    TicketType findTicketTypeById(Long ticketTypeId);
+    List<TicketType> listTicketType();
 
 }
